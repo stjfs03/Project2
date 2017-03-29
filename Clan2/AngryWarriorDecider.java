6c3e08b250e5d23035a6c0734885765ebb1f87d8
Mario <stmos01@moravian.edu>
2017-03-29 14:05:33 -0400

    Updated AngryWarriorDecider

--- Clan2/AngryWarriorDecider.java
+++ Clan2/AngryWarriorDecider.java
@@ -5,23 +5,33 @@ import clanmelee.ClanMember;
 import static clanmelee.ClanMember.ClanMemberType.WARRIOR;
 
 public class AngryWarriorDecider  implements ActionPointDecider {
+
     private int actionPoints;
 
     public AngryWarriorDecider(int actionPoints) {
         this.actionPoints = actionPoints;
     }
 
-    @Override
-    public int decideActionPoints(ClanMember self, ClanMember opponent) {
-        //Check if clan IDs match
-        boolean clanIDsMatch = self.getClanID() == opponent.getClanID();
 
-        //If opponent is in different clans --> fight
+    /**
+     *
+     * @param self The player
+     * @param target The character the player is interacting with.
+     *               If target is friendly, the player runs away.
+     *               If the target is an enemy, the player attacks.
+     * @return the amount of actionPoints used
+     */
+    @Override
+    public int decideActionPoints(ClanMember self, ClanMember target) {
+        //Check if clan IDs match. If clan ID's match, both are in the same clan.
+        boolean clanIDsMatch = self.getClanID() == target.getClanID();
+
+        //If opponent is in a different clan --> fight
         if (!clanIDsMatch) {
             return actionPoints;
         }
 
-        //If opponent is in same clan --> retreat
+        //If opponent is in the same clan --> retreat
         else {
             return 0;
         }
