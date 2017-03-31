6525549c6714926acb384425f8610ee84f1ead48
John Spirk <stjfs03@moravian.edu>
2017-03-31 12:51:02 -0400

    Pushing AssassinMember doc - John

--- Clan1/AssassinMember.java
+++ Clan1/AssassinMember.java
@@ -8,12 +8,25 @@ import java.util.ArrayList;
 
 import static clanmelee.ClanMember.ClanMemberType.WARRIOR;
 
+/**
+ * @Purpose - Defines the instantiation of the AssassinMember clan member
+ */
 public class AssassinMember extends Clan {
 
+    /**
+     * @param - clanID - number designated as clan ID
+     */
     public AssassinMember(int clanID) {
-        super("Assassin", clanID);
+        super("Clan1", clanID);
     }
 
+    /**
+     * This method creates an ArrayList and fills it with the desired number of Assassins
+     * (later to be added to the full clan members array? - According to Dr Schaper)
+     *
+     * @param hitPoints - The number of hit points to be distributed amongst all the clan members
+     * @return - an ArrayList of Assassins
+     */
     @Override
     public ArrayList<ClanMember> getClanMembers(int hitPoints) {
         ArrayList<ClanMember> clanMembers = new ArrayList<>();
