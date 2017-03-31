ac280f77c0734e5bd29dc9d0715a85c03f12524f
John Spirk <stjfs03@moravian.edu>
2017-03-31 11:07:01 -0400

    Pushing ClanStats Class - John

--- ClanStats.java
+++ ClanStats.java
@@ -3,11 +3,10 @@ package clanmelee;
 public class ClanStats {
     /**
      * @param totalClanCount - total number of clans in the melee
-     * @param hitPoints - array of total hitpoints in each clan
-     * @param playerCounts - array of total players
-     * @param warriorCounts -
-     * @param healerCounts -
-     *
+     * @param hitPoints - array of total hitpoints per clan
+     * @param playerCounts - array of total players in each clan
+     * @param warriorCounts - array of total warriors within each clan
+     * @param healerCounts - array of total healers within each clan
      */
     private final int totalClanCount;
     private int[] hitPoints;
@@ -18,7 +17,7 @@ public class ClanStats {
     /**
      * Constructor
      *
-     * @param clanCount -
+     * @param clanCount - Represents the number of clans in the melee
      */
     public ClanStats(int clanCount) {
         this.totalClanCount = clanCount;
@@ -29,75 +28,58 @@ public class ClanStats {
     }
 
     /**
+     * @param p - Adds a ClanMember object to clan
      *
-     * @param cm - adds ClanMember to clan
-     *
-     * This method
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
+     * This method assigns the clan member object a clanID,
+     * adds that member's hitpoints to the clan's hitpoint array,
+     * updates the clan's player count array, and updates the
+     * number of healers and warriors in the respective clan
+     * arrays
      */
-    public void addPlayer(ClanMember cm) {
-        int clanID = cm.getClanID();
-        hitPoints[clanID] += cm.getHitPoints();
+    public void addPlayer(ClanMember p) {
+        int clanID = p.getClanID();
+        hitPoints[clanID] += p.getHitPoints();
         playerCounts[clanID] += 1;
-        if (cm.getType() == ClanMember.ClanMemberType.HEALER)
+        if (p.getType() == ClanMember.ClanMemberType.HEALER)
             healerCounts[clanID] += 1;
-        else if (cm.getType() == ClanMember.ClanMemberType.WARRIOR)
+        else if (p.getType() == ClanMember.ClanMemberType.WARRIOR)
             warriorCounts[clanID] += 1;
     }
 
     /**
-     *
-     *
-     * @param clanID
-     * @return
+     * @param clanID - A clan's unique ID
+     * @return - The total hit points of a certain clan
      */
     public int getHitPoints(int clanID) {
         return hitPoints[clanID];
     }
 
     /**
-     *
-     *
-     * @param clanID
-     * @return
+     * @param clanID - A clan's unique ID
+     * @return - The number of players (members) in a certain clan
      */
     public int getPlayerCount(int clanID) {
         return playerCounts[clanID];
     }
 
     /**
-     *
-     *
-     * @param clanID
-     * @return
+     * @param clanID - A clan's unique ID
+     * @return - The number of warriors in a clan
      */
     public int getWarriorCount(int clanID) {
         return warriorCounts[clanID];
     }
 
     /**
-     *
-     *
-     * @param clanID
-     * @return
+     * @param clanID - A clan's unique ID
+     * @return The number of healers in a clan
      */
     public int getHealerCount(int clanID) {
         return healerCounts[clanID];
     }
 
     /**
-     *
-     *
-     * @return
+     * @return - The number of active clans (clans with more than 0 members)
      */
     public int getClanCount() {
         int clanCount = 0;
@@ -109,9 +91,7 @@ public class ClanStats {
     }
 
     /**
-     *
-     *
-     * @return
+     * @return - The winning clan (the clan with the most total hp)
      */
     public int getWinner() {
         int max = 0;
