5eb1007e1b0f9b0f69c81506710fa2acd9edbe16
John Spirk <stjfs03@moravian.edu>
2017-04-03 13:18:50 -0400

    Documented AssassinDecider - John

--- Clan1/AssassinDecider.java
+++ Clan1/AssassinDecider.java
@@ -6,28 +6,46 @@ import clanmelee.ClanMember;
 import static clanmelee.ClanMember.ClanMemberType.HEALER;
 import static clanmelee.ClanMember.ClanMemberType.WARRIOR;
 
+/**
+ * Implements ActionPointDecider interface and provides a strategy for
+ * an AssassinMember.
+ */
 public class AssassinDecider implements ActionPointDecider {
     private int actionPoints;
 
+    /**
+     * @param actionPoints - how much damage the assassin can do
+     */
     public AssassinDecider(int actionPoints) {
         this.actionPoints = actionPoints;
     }
+
+    /**
+     * This method checks to see if the confronting clan members are of the same clan before making a move.
+     * If the IDs match: return 0. If the IDs are different: the assassin will attack.
+     *
+     * @param me - Our Assassin clan member
+     * @param other - Our opponent
+     * @return - Amount of damage to be dealt to 'other'
+     */
     @Override
     public int decideActionPoints(ClanMember me, ClanMember other) {
         boolean clanIDsMatch = me.getClanID() == other.getClanID();
 
         if (!clanIDsMatch && other.getType() == WARRIOR) {
+            //if warrior's hp is <= a third of his max health: deal 3x damage?
             if (other.getHitPoints() <= (other.getMaxHitPoints() / 3)) {
                 return actionPoints * 3;
             }
-            else {
+            else { //else deal normal damage
                 return actionPoints;
             }
         }
-
+        //if the opponent is a healer, deal 2x damage?
         if (!clanIDsMatch && other.getType() == HEALER) {
             return actionPoints * 2;
         }
+        //return 0 if the clan IDs match
         return  0;
     }
 }
