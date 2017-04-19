aeb8427b6a62554ccc97d3fc04b264f01f2f9fe7
Justin Madilia <stjpm09@moravian.edu>
2017-04-19 11:11:16 -0400

    Why won't this work (????)

--- clanmelee/Madilia/MadiliaClan.java
+++ clanmelee/Madilia/MadiliaClan.java
@@ -26,50 +26,24 @@ public class MadiliaClan extends Clan {
 
         int totalHP = hitPoints;
         int unassignedHP = hitPoints;
+        int nextHpAssignment;
 
-        if ( totalHP < 5000 ){
-            while ( unassignedHP > 0){
-                int nextWarriorHP = 25;
-                if (unassignedHP < 25){
-                    nextWarriorHP = unassignedHP;
-                }
-                clanMembers.add(new ClanMember(getClanID(), WARRIOR, nextWarriorHP, badWarriorDecider));
-//                clanMembers.add(new ClanMember(getClanID(), WARRIOR, nextWarriorHP, regularDecider));
+        while (unassignedHP > 0) {
+            nextHpAssignment = totalHP/10;
+            if (unassignedHP < totalHP/10) {
+                nextHpAssignment = unassignedHP;
             }
 
-            while (unassignedHP > 0) {
-                int nextHealerHPAssignment = 25;
-                if (unassignedHP < 25) {
-                    nextHealerHPAssignment = unassignedHP;
+            if (Math.random() > 0.5) {
+                if (Math.random() > 0.5) {
+                    clanMembers.add(new ClanMember(getClanID(), WARRIOR, nextHpAssignment, badWarriorDecider));
                 }
-                clanMembers.add(new ClanMember(getClanID(), HEALER, nextHealerHPAssignment, healerHealerDecider));
-//                clanMembers.add(new ClanMember(getClanID(), HEALER, nextHealerHPAssignment, assistantDecider));
 
-                unassignedHP -= nextHealerHPAssignment;
-            }
-        }
-        else {
-            while (unassignedHP > 0) {
-                int nextWarriorHPAssignment = 5000;
-                if (unassignedHP < 5000) {
-                    nextWarriorHPAssignment = unassignedHP;
+                else {
+                    clanMembers.add(new ClanMember(getClanID(), HEALER, nextHpAssignment, healerHealerDecider));
                 }
-//                clanMembers.add(new ClanMember(getClanID(), WARRIOR, nextWarriorHPAssignment, braveDecider));
-//                clanMembers.add(new ClanMember(getClanID(), WARRIOR, nextWarriorHPAssignment, regularDecider));
-
-                unassignedHP -= nextWarriorHPAssignment;
             }
-
-            while (unassignedHP > 0) {
-                int nextHealerHPAssignment = 5000;
-                if (unassignedHP < 5000) {
-                    nextHealerHPAssignment = unassignedHP;
-                }
-//                clanMembers.add(new ClanMember(getClanID(), HEALER, nextHealerHPAssignment, doctorDecider));
-//                clanMembers.add(new ClanMember(getClanID(), HEALER, nextHealerHPAssignment, assistantDecider));
-
-                unassignedHP -= nextHealerHPAssignment;
-            }
+            unassignedHP -= nextHpAssignment;
         }
 
         return clanMembers;
