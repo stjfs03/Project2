f023c604765ab30aa5975def99fc3a8f75f3e3d2
John Spirk <stjfs03@moravian.edu>
2017-04-19 10:28:09 -0400

    re-adding my clan class for some reason -John

--- clanmelee/Spirk/spirkClan.java
+++ clanmelee/Spirk/spirkClan.java
@@ -1,7 +1,29 @@
 package clanmelee.Spirk;
 
-/**
- * Created by JohnSpirk on 4/18/17.
- */
-public class spirkClan {
+import clanmelee.Clan;
+import clanmelee.ClanMember;
+import java.util.ArrayList;
+
+public class SpirkClan extends Clan {
+
+    public SpirkClan(int clanID) {
+        super("SpirkClan", clanID);
+    }
+
+    @Override
+    public ArrayList<ClanMember> getClanMembers(int hitPoints) {
+        ArrayList<ClanMember> fullClanMembers = new ArrayList<>();
+
+        ConfusedHealerMember conHealer = new ConfusedHealerMember(getClanID());
+        MedicHunterMember medHunter = new MedicHunterMember(getClanID());
+        JediKnightMember jediKnight = new JediKnightMember(getClanID());
+        DarthPlagueisTheWiseMember sithLord = new DarthPlagueisTheWiseMember(getClanID());
+
+        fullClanMembers.addAll(conHealer.getClanMembers(hitPoints));
+        fullClanMembers.addAll(medHunter.getClanMembers(hitPoints));
+        fullClanMembers.addAll(jediKnight.getClanMembers(hitPoints));
+        fullClanMembers.addAll(sithLord.getClanMembers(hitPoints));
+
+        return fullClanMembers;
+    }
 }
